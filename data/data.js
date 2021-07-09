945a15df388667e9b4881c3125a52566207f4307
赵文虎 <1247615020@qq.com>
2021-07-09 18:07:21 +0800

    添加欢迎页 列表页 和详情页

---
+++ data/data.js
@@ -0,0 +1,28 @@
+var postList = [{
+  author:"/images/avatar/head.png",
+  date:"2021-07-07",
+  title:"2020年样品展示",
+  image:"/images/avatar/1.jpg",
+  content:"服装样品是指能够代表服装品质的实物。服装样品代表着服装这一商品的具体实样，是服装生产的标准。用它来确定批量服装的外观造型、品质质地、视觉色彩、结构工艺等内容。",
+  number:{
+    favor:"300",
+    look:"358"
+  },
+  flag:true
+},{
+  author:"/images/avatar/head.png",
+  date:"2021-07-08",
+  title:"2021年样品展示",
+  image:"/images/avatar/2.jpg",
+  content:"服装样品是指能够代表服装品质的实物。服装样品代表着服装这一商品的具体实样，是服装生产的标准。用它来确定批量服装的外观造型、品质质地、视觉色彩、结构工艺等内容。",
+  number:{
+    favor:"400",
+    look:"458"
+  },
+  flag:false
+}]
+
+
+export{
+  postList
+}
---
+++ pages/posts/posts.js
@@ -0,0 +1,78 @@
+// pages/posts/posts.js
+
+import {postList} from '../../data/data.js'
+console.log(postList)
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      postList
+    })
+  },
+
+  onGoToDetail(event){
+    wx.navigateTo({
+      url: '/pages/post-detail/post-detail',
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    console.log("onReady")
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    console.log("onShow")
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    console.log("onHide")
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    console.log("onUnload")
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    console.log("onPullDownRefresh")
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    console.log("onReachBottom")
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    console.log("onShareAppMessage")
+  }
+})
